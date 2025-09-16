dc6fde351cd0af44ada902abf41704b064934ffb
vongotha0@gmail.com <vongotha0@gmail.com>
2025-09-16 02:46:31 +0100

    Day 4: Baby Steps -- Make a pretty UI Template

--- learn_Laravel/resources/views/components/layout.blade.php
+++ learn_Laravel/resources/views/components/layout.blade.php
@@ -1,29 +1,112 @@
 <!DOCTYPE html>
-<html lang="en">
+<html class="h-full bg-gray-900" lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Home Page</title>
-        <style>
-            body {
-                margin: 0;
-                display: flex;
-                flex-direction: column;
-                justify-content: space-around;
-                align-content: center
-            }        
-        </style>
+        <script src="https://cdn.tailwindcss.com"></script>
     </head>
-    <body>
-        <nav>
-            <ul>
-                <a href="/">Home</a>
-                <a href="/about">About Us</a>
-                <a href="/contact">Contacts</a>
-            </ul>
+    <body class="h-full">
+        <!-- Include this script tag or install `@tailwindplus/elements` via npm: -->
+        <!-- <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script> -->
+        <!--
+        This example requires updating your template:
+
+        ```
+        <html class="h-full bg-gray-900">
+        <body class="h-full">
+        ```
+        -->
+        <div class="min-h-full">
+        <nav class="bg-gray-800/50">
+            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div class="flex h-16 items-center justify-between">
+                <div class="flex items-center">
+                <div class="shrink-0">
+                    <img src="https://laracasts.com/images/logo/logo-triangle.svg" alt="Your Company" class="size-8" />
+                </div>
+                <div class="hidden md:block">
+                    <div class="ml-10 flex items-baseline space-x-4">
+                    <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
+                <a href="/" aria-current="page" class="block rounded-md bg-gray-950/50 px-3 py-2 text-base font-medium text-white">Home</a>
+                <a href="/about" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
+                <a href="/contact" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>                    </div>
+                </div>
+                </div>
+                <div class="hidden md:block">
+                <div class="ml-4 flex items-center md:ml-6">
+                    <button type="button" class="relative rounded-full p-1 text-gray-400 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500">
+                    <span class="absolute -inset-1.5"></span>
+                    <span class="sr-only">View notifications</span>
+                    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6">
+                        <path d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                    </button>
+
+                    <!-- Profile dropdown -->
+                    <el-dropdown class="relative ml-3">
+                    <button class="relative flex max-w-xs items-center rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+                        <span class="absolute -inset-1.5"></span>
+                        <span class="sr-only">Open user menu</span>
+                        <img src="https://laracasts.com/images/lary-ai-face.svg" alt="" class="size-8 rounded-full outline -outline-offset-1 outline-white/10" />
+                    </button>
+                    </el-dropdown>
+                </div>
+                </div>
+                <div class="-mr-2 flex md:hidden">
+                <!-- Mobile menu button -->
+                <button type="button" command="--toggle" commandfor="mobile-menu" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500">
+                    <span class="absolute -inset-0.5"></span>
+                    <span class="sr-only">Open main menu</span>
+                    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6 in-aria-expanded:hidden">
+                    <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6 not-in-aria-expanded:hidden">
+                    <path d="M6 18 18 6M6 6l12 12" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                </button>
+                </div>
+            </div>
+            </div>
+
+            <el-disclosure id="mobile-menu" hidden class="block md:hidden">
+            <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
+                <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
+                <a href="/" aria-current="page" class="block rounded-md bg-gray-950/50 px-3 py-2 text-base font-medium text-white">Home</a>
+                <a href="/about" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
+                <a href="/contact" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
+            </div>
+            <div class="border-t border-white/10 pt-4 pb-3">
+                <div class="flex items-center px-5">
+                <div class="shrink-0">
+                    <img src="https://laracasts.com/images/lary-ai-face.svg" alt="" class="size-10 rounded-full outline -outline-offset-1 outline-white/10" />
+                </div>
+                <div class="ml-3">
+                    <div class="text-base/5 font-medium text-white">Lary robot</div>
+                    <div class="text-sm font-medium text-gray-400">lary@laracasts.com</div>
+                </div>
+                <button type="button" class="relative ml-auto shrink-0 rounded-full p-1 text-gray-400 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500">
+                    <span class="absolute -inset-1.5"></span>
+                    <span class="sr-only">View notifications</span>
+                    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6">
+                    <path d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                </button>
+                </div>
+            </div>
+            </el-disclosure>
         </nav>
+
+        <header class="relative bg-gray-800 after:pointer-events-none after:absolute after:inset-x-0 after:inset-y-0 after:border-y after:border-white/10">
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <h1 class="text-3xl font-bold tracking-tight text-white">{{ $heading }}</h1>
+            </div>
+        </header>
         <main>
-            {{ $slot }}
+            <div class="mx-auto text-white text-2xs max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                {{ $slot }}
+            </div>
         </main>
+        </div>
     </body>
     </html>
