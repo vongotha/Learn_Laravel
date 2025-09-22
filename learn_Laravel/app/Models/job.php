f8965c27003f0ff80e88b986e32ef3af0593666e
vongotha0@gmail.com <vongotha0@gmail.com>
2025-09-22 23:22:59 +0100

    Eloquent -- Meet Eloquent

--- learn_Laravel/app/Models/job.php
+++ learn_Laravel/app/Models/job.php
@@ -6,23 +6,7 @@ use Illuminate\Support\Arr;
 
 class Job {
     public static function all() : array {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Manager',
-                'salary' => '$30,000',
-            ],
-        ];
+        //
     }
 
     public static function find($id): array {
